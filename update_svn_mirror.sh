1caa2e913025b7fce6ca59309538d36cf48ed064
Robert Buels <rmb32@cornell.edu>
2011-04-13 09:39:06 -0700

    tweak mirror updater to stop on error

--- update_svn_mirror.sh
+++ update_svn_mirror.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rsync -avz --compress-level=9 gmod.svn.sourceforge.net::svn/gmod/* svn_mirror/
+set -e
+rsync -avz --delete --compress-level=9 gmod.svn.sourceforge.net::svn/gmod/* svn_mirror/
 touch svn_mirror/update_stamp
